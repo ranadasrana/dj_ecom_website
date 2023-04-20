774e98c125e9b8124173ff669567c42a5fa76973
kowshik <kowshik@gmail.com>
2023-04-20 13:47:28 +0600

    home page dynamic

--- E_website/product/models.py
+++ E_website/product/models.py
@@ -15,8 +15,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    title = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(unique=True, max_length=250)
     featured = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     thumbnail = models.URLField()
--- E_website/product/views.py
+++ E_website/product/views.py
@@ -1,8 +1,24 @@
 from django.views import generic
+from .models import(
+    Category,
+    Product, 
+    Slider
+)
 
 # Create your views here.
 class Home(generic.TemplateView):
     template_name='home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                'featured_categories': Category.objects.filter(featured=True),
+                'featured_products': Product.objects.filter(featured=True),
+                'sliders': Slider.objects.filter(show=True)
+            }
+        )
+        return context
 
 class ProductDetails(generic.TemplateView):
     template_name='product/product-details.html'
